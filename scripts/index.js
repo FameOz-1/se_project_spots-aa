de25e0d1807dffc6bbc8bd56db16a65f00bedc5b
AUSTIN ABBOTT <AUSTIN.GREGORY.ABBOTT@OUTLOOK.COM>
2025-04-19 13:21:44 -0700

    Modals Fin. +Transitions

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#post-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#post-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +36,23 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  const newPostImage = newPostImageInput.value;
+  const newPostCaption = newPostCaptionInput.value;
+  console.log(newPostImage, newPostCaption);
+  newPostModal.classList.remove("modal_is-opened");
+  newPostForm.reset(); // Reset the form fields after submission
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
