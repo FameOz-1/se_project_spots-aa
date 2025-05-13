35f59fa12062ff60e83617d168b751a4a46153d5
AUSTIN ABBOTT <AUSTIN.GREGORY.ABBOTT@OUTLOOK.COM>
2025-05-13 05:25:05 -0700

    SPOTS_P.6/Revised-Edits

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,55 +1,3 @@
-// const showInputError = (formElement, inputElement, errorMessage) => {
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   inputElement.classList.add("form__input_type_error");
-//   errorElement.textContent = errorMessage;
-//   errorElement.classList.add("form__input-error_active");
-// };
-
-// const hideInputError = (formElement, inputElement) => {
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   inputElement.classList.remove("form__input_type_error");
-//   errorElement.classList.remove("form__input-error_active");
-//   errorElement.textContent = "";
-// };
-
-// const checkInputValidity = (formElement, inputElement) => {
-//   if (!inputElement.validity.valid) {
-//     showInputError(formElement, inputElement, inputElement.validationMessage);
-//   } else {
-//     hideInputError(formElement, inputElement);
-//   }
-// };
-
-// const hasInvalidInput = (inputList) => {
-//   return inputList.some((inputElement) => {
-//     return !inputElement.validity.valid;
-//   });
-// };
-
-// const toggleButtonState = (inputList, buttonElement) => {
-//   console.log(hasInvalidInput(inputList));
-//   if (hasInvalidInput(inputList)) {
-//     buttonElement.classList.add("button_inactive");
-//   } else {
-//     buttonElement.classList.remove("button_inactive");
-//   }
-// };
-
-// const setEventListeners = (formElement) => {
-// const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-// const buttonElement = formElement.querySelector(".form__submit");
-
-// toggleButtonState(inputList, buttonElement);
-
-// inputList.forEach((inputElement) => {
-//   inputElement.addEventListener("input", function () {
-//     checkInputValidity(formElement, inputElement);
-
-//     toggleButtonState(inputList, buttonElement);
-//   });
-// });
-// };
-
 const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
@@ -89,17 +37,16 @@ const hasInvalidInput = (inputList) => {
 
 const toggleButtonState = (inputList, buttonEl, config) => {
   if (hasInvalidInput(inputList)) {
-    disableButton(buttonEl);
-    buttonEl.classList.add(config.inactiveButtonClass);
+    disableButton(buttonEl, config);
   } else {
     buttonEl.disabled = false;
     buttonEl.classList.remove(config.inactiveButtonClass);
   }
 };
 
-const disableButton = (buttonEl) => {
+const disableButton = (buttonEl, config) => {
   buttonEl.disabled = true;
-  buttonEl.classList.add(settings.inactiveButtonClass);
+  buttonEl.classList.add(config.inactiveButtonClass);
 };
 
 const resetValidation = (formEl, inputList, config) => {
